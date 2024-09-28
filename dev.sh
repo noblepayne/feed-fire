189cea3cbe5b939f072bfc09bfbba51041c50aa6
Wes Payne <noblepayne@noblepayne.com>
2024-09-28 15:22:21 -0700

    add .env template and update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env sh
 nix develop --impure path:. -c $SHELL
+# N.B.
+# --impure  - allows devenv to access state data when running with flakes
+# path:.    - allows devenv to access a .env file (if it exists) when not in the repo
+# -c $SHELl - support fish, zsh, etc. (default shell is bash)
